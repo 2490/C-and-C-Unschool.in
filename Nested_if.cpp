7e5f9a786441f0395bbf8e703d042c42bbb44427
2490 <68751457+2490@users.noreply.github.com>
2020-07-27 18:52:36 +0530

    Add files via upload

---
+++ Nested_if.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+int main()
+{ int x;
+	printf("Enter the percentage-");
+	scanf("%d" ,&x);
+
+	if(x>80)
+	{
+		printf("A grade ");
+}
+		if(x>=70 && x<80)
+		{
+		  printf("B grade")	;
+	     }
+		  if(x>=60 && x<70)
+		  {	 
+		  printf("C grade ");
+	} 
+		  	if(x>=45 && x<60)
+		  	{
+		  	
+			  printf("D grade");
+		      }
+			  if(x<45)
+			  {
+			  	printf("Fail");
+			  	
+			  }
+			  return 0;
+			
+}
+		
+	
+
+
