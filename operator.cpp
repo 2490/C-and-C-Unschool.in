2a4665bfe6d5cd76f4a51bc385d593a87dbbbfbb
2490 <68751457+2490@users.noreply.github.com>
2020-07-27 18:32:05 +0530

    Add files via upload
    Assing1-Operator

---
+++ operator.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	float x,y;
+	printf("Enter the 2 nos- ");
+	
+	scanf("%f %f",&x,&y);
+		
+		printf("sum of %f and %f is %f is \n",x,y,x+y);
+			printf("Substraction of %f and %f is %f is \n",x,y,x-y);
+				printf("division of %f and %f is-%f is\n",x,y,x/y);
+					printf("Multiplicatin of %f and %f is %f is \n",x,y,x*y);
+	return 0;				
+}
