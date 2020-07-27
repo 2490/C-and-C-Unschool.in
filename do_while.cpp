aac74b601db3fe320383f78b2641d44e7fe1be73
2490 <68751457+2490@users.noreply.github.com>
2020-07-27 18:48:46 +0530

    Add files via upload

---
+++ do_while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ int main()
+ { int sum,x=0;
+ 		
+ 	do{
+ 		printf("Enter the nos-");
+ 		scanf("%d",&x);
+ 		sum=sum+x;
+	 }
+	 while(x!=0);
+	 
+	 printf("Sum=%d",sum);
+ 	
+ }
