9dc5f7c983418388072546cab71a89b4c940784c
2490 <68751457+2490@users.noreply.github.com>
2020-08-13 13:41:56 +0530

    Add files via upload

---
+++ STRCPY.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char S1[]="Sourav";
+	char S2[]="Unschool";
+	char S3[30];
+	char S4[30];
+	char S5[]="FGH";
+	
+	strcpy(S1,S2);
+	strcpy(S4,"COPY SUCCESFUL");
+	strcpy(S3,S5);
+	
+	printf(" s1:%s \n s2:%s \n s3:%s \n s4: %s \n s5:%s",S1,S2,S3,S4,S5;
+	
+
+	return 0;
+	
+}
+
