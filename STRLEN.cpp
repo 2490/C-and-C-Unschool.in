1275f66410cdf432bd8edfb93dec61ddf79ba872
2490 <68751457+2490@users.noreply.github.com>
2020-08-13 13:43:30 +0530

    Add files via upload

---
+++ STRLEN.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	
+	printf("Enter a String:");
+	gets(s);
+printf("THE LENGTH OF STRING %s :%d",s,strlen(s));//REVERSES THE STRING AND RETUR IT
+return 0;
+}
